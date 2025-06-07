e4b8ec3685b2a186abe8da9b0055e1e1116ae732
tirosko <tibor.rosko@hotmail.com>
2025-06-07 10:04:49 +0200

    žltý konic hry

---
+++ SRC/DU/koniec.py
@@ -0,0 +1,143 @@
+# https://www.geeksforgeeks.org/snake-game-in-python-using-pygame-module/
+# importing libraries
+
+import pygame
+import time
+import random
+from pygame.locals import QUIT
+import sys
+
+snake_speed = 10
+
+# Window size
+window_x = 720
+window_y = 480
+
+# defining colors
+black = pygame.Color(0, 0, 0)
+white = pygame.Color(255, 255, 255)
+red = pygame.Color(255, 0, 0)
+green = pygame.Color(0, 255, 0)
+blue = pygame.Color(0, 0, 255)
+
+# defining snake default position
+snake_position = [100, 50]
+
+# defining first 4 blocks of snake body
+snake_body = [[100, 50],
+              [90, 50],
+              [80, 50],
+              [70, 50]
+              ]
+
+direction = 'RIGHT'
+change_to = direction
+
+fruit_spawn = True
+
+# Initialising pygame
+pygame.init()
+
+# Initialise game window
+pygame.display.set_caption('Červik podľa Jakuba')
+game_window = pygame.display.set_mode((window_x, window_y))
+
+# FPS (frames per second) controller
+fps = pygame.time.Clock()
+
+fruit_position = [random.randrange(1, (window_x//10)) * 10,
+                  random.randrange(1, (window_y//10)) * 10]
+
+
+def game_over():
+    # Vytvorenie fontu pre text "Game Over"
+    font = pygame.font.SysFont('times new roman', 50)
+    # game_over_surface = font.render(
+    #     'Your Score is : ' + str(score), True, red)
+    game_over_surface = font.render('Koniec hry', True, "yellow")
+    game_over_rect = game_over_surface.get_rect()
+    game_over_rect.midtop = (window_x / 2, window_y / 4)
+    game_window.fill(black)
+    game_window.blit(game_over_surface, game_over_rect)
+    pygame.display.flip()
+    time.sleep(2)
+    pygame.quit()
+    sys.exit()
+
+
+while True:
+
+    # handling key events
+    for event in pygame.event.get():
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_UP:
+                change_to = 'UP'
+            if event.key == pygame.K_DOWN:
+                change_to = 'DOWN'
+            if event.key == pygame.K_LEFT:
+                change_to = 'LEFT'
+            if event.key == pygame.K_RIGHT:
+                change_to = 'RIGHT'
+
+    # Dôležité vysvetliť, že had sa môže pohybovať len jedným smerom
+    # Ak sa stlačí klávesa, zmení sa smer pohybu. ale nemôže sa pohybovať dvoma smermi naraz a teda napríklad do protismeru
+    # Ak sú stlačené dve klávesy súčasne nechceme, aby sa had pohyboval dvoma smermi naraz
+    if change_to == 'UP' and direction != 'DOWN':
+        direction = 'UP'
+    if change_to == 'DOWN' and direction != 'UP':
+        direction = 'DOWN'
+    if change_to == 'LEFT' and direction != 'RIGHT':
+        direction = 'LEFT'
+    if change_to == 'RIGHT' and direction != 'LEFT':
+        direction = 'RIGHT'
+
+    # Moving the snake
+    # Ak sa had pohybuje tak sa pohybuje o 10 pixelov v smere, ktorý je nastavený
+    if direction == 'UP':
+        snake_position[1] -= 10
+    if direction == 'DOWN':
+        snake_position[1] += 10
+    if direction == 'LEFT':
+        snake_position[0] -= 10
+    if direction == 'RIGHT':
+        snake_position[0] += 10
+
+    snake_body.insert(0, list(snake_position))
+    if snake_position[0] == fruit_position[0] and snake_position[1] == fruit_position[1]:
+        # score += 10
+        fruit_spawn = False
+    else:
+        snake_body.pop()
+
+    if not fruit_spawn:
+        fruit_position = [random.randrange(1, (window_x//10)) * 10,
+                          random.randrange(1, (window_y//10)) * 10]
+    fruit_spawn = True    
+    game_window.fill(black)
+
+
+
+    pygame.draw.rect(game_window, red, pygame.Rect(
+        fruit_position[0], fruit_position[1], 10, 10))
+    for pos in snake_body:
+        pygame.draw.rect(game_window, green, pygame.Rect(
+            pos[0], pos[1], 10, 10))
+
+    if snake_position[1] < 0 or snake_position[0] < 0:
+        game_over()
+    if snake_position[0] > window_x-10 or snake_position[1] > window_y-10:
+        game_over() 
+
+    # Touching the snake body
+    for block in snake_body[1:]:
+        if snake_position[0] == block[0] and snake_position[1] == block[1]:
+            game_over()
+
+    # Refresh game screen
+    pygame.display.update()
+    # Frame Per Second /Refresh Rate
+    fps.tick(snake_speed)
+
+    if event.type == QUIT:
+        pygame.quit()
+        sys.exit()
